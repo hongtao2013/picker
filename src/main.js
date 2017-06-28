2b5d6664d3bd33e5c02926cb04ace2a957731312
hongtao <hongtaobj2014@gmail.com>
2017-06-28 15:41:35 +0800

    main修改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /**
  * 使用npm集成是时候使用
  */
-require('./../dist/picker.css');
-module.exports = require('./../dist/picker');
+require('./../dist/my-picker.css');
+module.exports = require('./../dist/my-picker');
